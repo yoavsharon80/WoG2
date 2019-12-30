6b73df4f09ff5e3afbb633bdbeb500043db55ee0
yoavsharon80 <yoav.sharon80@gmail.com>
2019-12-30 21:02:01 +0200

    version 1.2 change validation condition

--- MainScores.py
+++ MainScores.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def score_server():
     try:
-        score = open(Utils.SCORES_FILE_NAME(), "r")
+        score = open('scores.txt', "r")
     except BaseException as e:
         return """<html>
         <head>
